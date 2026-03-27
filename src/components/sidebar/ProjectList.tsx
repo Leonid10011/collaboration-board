bdc39dc2b539a78966d83c013ecba4706250ef5b
Leonid10011 <leonid@budkov@gmail.com>
2026-03-27 15:44:44 +0100

    feat(sidebar): add ProjectList component

---
+++ src/components/sidebar/ProjectList.tsx
@@ -0,0 +1,21 @@
+import { Project } from "@/domain/projects";
+
+type ProjectListType = {
+  projects: Project[];
+};
+
+export default function ProjectList({ projects }: ProjectListType) {
+  return (
+    <div className="flex flex-col gap-4">
+      {projects.map((p) => (
+        <div
+          className="flex flex-col rounded px-2 py-4 bg-foreground hover:cursor-pointer"
+          key={p.id}
+        >
+          <h3>{p.title}</h3>
+          <p>{p.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
