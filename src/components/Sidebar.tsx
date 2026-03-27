11b7c042765e3426dfaf07e4b4a3e4786e569be5
Leonid10011 <leonid@budkov@gmail.com>
2026-03-27 16:15:09 +0100

    feat(sidebar): add ui actions and projects block

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,29 @@
+import { useProject } from "@/context/ProjectContext";
+import InfoBlock from "./ui/InfoBlock";
+import ProjectList from "./sidebar/ProjectList";
+
 export default function Sidebar() {
+  const { userProjects } = useProject();
+
   return (
-    <div className="flex flex-col px-2 py-4 min-w-[200px] border-r-2 border-solid bg-background">
-      {"Sidebar"}
+    <div className="flex flex-col px-2 py-4 min-w-[200px] gap-4 border-r-2 border-solid bg-background">
+      <InfoBlock
+        title="Actions"
+        content={
+          <button
+            className="px-2 py-4 bg-foreground rounded text-start hover:cursor-pointer"
+            onClick={() => {
+              console.log("Create Button onClick");
+            }}
+          >
+            Create Project
+          </button>
+        }
+      />
+      <InfoBlock
+        title="Projects"
+        content={<ProjectList projects={userProjects} />}
+      />
     </div>
   );
 }
