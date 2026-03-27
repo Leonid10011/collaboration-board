08c2f1b3907938d9d3b65480a78b3176cace9f50
Leonid10011 <leonid@budkov@gmail.com>
2026-03-27 17:52:40 +0100

    feat(taskboard): add Column call with mock data

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -1,7 +1,40 @@
+import Column from "./taskboard/Column";
+
 export default function TaskBoard() {
+  const mockColumns = [
+    {
+      id: 1,
+      statusColor: "gray",
+      name: "Backlog",
+      count: 3,
+    },
+    {
+      id: 2,
+      statusColor: "yellow",
+      name: "In Progress",
+      count: 2,
+    },
+    {
+      id: 3,
+      statusColor: "green",
+      name: "Done",
+      count: 5,
+    },
+  ];
+
   return (
-    <div className="flex flex-col flex-1 h-full">
+    <div className="flex flex-col flex-1 gap-4 px-4 py-2">
       <h1>Task Board</h1>
+      <div className="flex flex-row gap-x-8 h-full">
+        {mockColumns.map((c) => (
+          <Column
+            key={c.id}
+            statusColor={c.statusColor}
+            name={c.name}
+            count={c.count}
+          />
+        ))}
+      </div>
     </div>
   );
 }
