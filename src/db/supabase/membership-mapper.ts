64c99a97ca860c89dc2d50e80ee268accd7449d3
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-27 12:43:50 +0100

    feat(db): add mapping function from MembershipDB to Membership

--- src/db/supabase/membership-mapper.ts
+++ src/db/supabase/membership-mapper.ts
@@ -1,4 +1,5 @@
-import { MembershipWithProfileDB } from "./membership-db";
+import { Membership } from "@/domain/memberships";
+import { MembershipDB, MembershipWithProfileDB } from "./membership-db";
 import { Member } from "@/domain/profiles";
 
 export function mapMembershipWithProfileDBToMember(
@@ -10,5 +11,17 @@ export function mapMembershipWithProfileDBToMember(
     imgUrl: membership.profiles.img_url,
     projectId: membership.project_id,
     projectRole: membership.role,
+  };
+}
+
+export function mapMembershipDBToMembership(
+  membership: MembershipDB,
+): Membership {
+  return {
+    id: membership.id,
+    projectId: membership.project_id,
+    userId: membership.user_id,
+    role: membership.role,
+    createdAt: new Date(membership.created_at),
   };
 }
