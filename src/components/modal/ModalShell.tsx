fe157e7d0ffc6421ce7a4cb2f3f59527425fd9aa
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-29 13:21:14 +0200

    feat(ui): add ModalShell component

--- src/components/modal/ModalShell.tsx
+++ src/components/modal/ModalShell.tsx
@@ -1,35 +1,46 @@
 type ModalShellProps = {
   title: string;
   children: React.ReactNode;
-  onClose?: () => void;
+  onClose: (value: boolean) => void;
+  confirmLabel?: string;
+  onConfirm: () => void;
 };
 
 export default function ModalShell({
   title,
   children,
   onClose,
+  confirmLabel,
+  onConfirm,
 }: ModalShellProps) {
+  const handleClose = () => {
+    onClose(false);
+  };
+
+  const handleConfirm = () => {
+    onConfirm();
+    onClose(false);
+  };
+
   return (
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
-      role="dialog"
-      aria-modal="true"
-      aria-labelledby="modal-title"
-    >
-      <div className="bg-background rounded shadow-lg w-full max-w-md p-4 relative">
-        {onClose && (
-          <button
-            onClick={onClose}
-            className="absolute top-2 right-2 text-meta hover:text-meta/90"
-            aria-label="Close modal"
-          >
-            x
-          </button>
-        )}
-        <h3 id="modal-title" className="text-lg font-semibold mb-2">
-          {title}
-        </h3>
+    <div role="dialog" aria-modal="true" aria-labelledby="modal-title">
+      <div
+        className="fixed inset-0  backdrop-blur-md flex items-center justify-center"
+        onClick={handleClose}
+      />
+      <div className="bg-main-1 border-2 rounded-md shadow-lg w-full max-w-2xl p-16 pb-32 z-10 fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <input
+          placeholder={title}
+          className="w-full text-4xl font-bold placeholder-gray-300 border-none outline-none bg transparent focus:ring-0 mb-8"
+          onChange={() => {}}
+        />
         <div className="flex-1 w-full">{children}</div>
+        <button
+          className="absolute bottom-8 right-16 bg-main-2 px-4 py-2 rounded-md hover:cursor-pointer"
+          onClick={handleConfirm}
+        >
+          {confirmLabel ? confirmLabel : "Confirm"}
+        </button>
       </div>
     </div>
   );
