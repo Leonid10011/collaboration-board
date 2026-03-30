cf588b0d6a054dcf2a78588e61e5c2bd54e3dad0
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-30 16:15:32 +0200

    feat(taskboard-column): add tasks as argument + remove mock data

--- src/components/taskboard/Column.tsx
+++ src/components/taskboard/Column.tsx
@@ -1,31 +1,13 @@
+import { Task } from "@/domain/tasks";
 import { Circle, SquarePlus } from "lucide-react";
 
 type ColumnProp = {
   statusColor: string;
   name: string;
-  count: number;
+  tasks: Task[];
 };
 
-/* Example Tasks. Will be replaced with context data */
-const mockTasks = [
-  {
-    id: 1,
-    title: "Task 1",
-    description: "Description for Task 1",
-  },
-  {
-    id: 2,
-    title: "Task 2",
-    description: "Description for Task 2",
-  },
-  {
-    id: 3,
-    title: "Task 3",
-    description: "Description for Task 3",
-  },
-];
-
-export default function Column({ statusColor, name, count }: ColumnProp) {
+export default function Column({ statusColor, name, tasks }: ColumnProp) {
   return (
     <div className="flex flex-col bg-main-2 h-full min-w-[220px] sm:min-w-[260x] md:min-w-[300px] flex-1 rounded px-4 py-2">
       {/* Column Header*/}
@@ -33,7 +15,7 @@ export default function Column({ statusColor, name, count }: ColumnProp) {
         <div className="flex flex-row gap-2 justify-start items-center">
           <Circle width={24} height={24} color={statusColor} />
           <span className="text-lg">{name}</span>
-          <span className="text-sm">{"(" + count + ")"}</span>
+          <span className="text-sm">{"(" + tasks.length + ")"}</span>
         </div>
         <SquarePlus
           width={32}
@@ -46,7 +28,7 @@ export default function Column({ statusColor, name, count }: ColumnProp) {
       </div>
       <div className="">
         {/* Tasks */}
-        {mockTasks.map((t) => (
+        {tasks.map((t) => (
           <div
             key={t.id}
             className="flex flex-col rounded px-2 py-4 bg-background mt-4 hover:cursor-pointer"
