eae0d3b77307fad8c6fcca7c73b790323580da3a
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-30 13:31:57 +0200

    feat(project-list): pass changeSelectedProject to ProjectItem

--- src/components/sidebar/ProjectList.tsx
+++ src/components/sidebar/ProjectList.tsx
@@ -1,19 +1,22 @@
 import { Project } from "@/domain/projects";
 import ProjectItem from "./projectList/ProjectItem";
+import { useProject } from "@/context/ProjectContext";
 
 type ProjectListType = {
   projects: Project[];
 };
 
 export default function ProjectList({ projects }: ProjectListType) {
+  const { changeSelectedProject } = useProject();
+
   return (
     <div className="flex flex-col gap-4">
       {projects.map((p) => (
         <ProjectItem
           key={p.id}
-          id={p.id}
           title={p.title}
           description={p.description}
+          onClick={() => changeSelectedProject(p.id)}
         />
       ))}
     </div>
