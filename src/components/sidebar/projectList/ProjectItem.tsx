cb99a460c8eae65ede6fea2c91330a815c0b5674
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-30 13:31:14 +0200

    feat(project-item): remove id + add onClick

--- src/components/sidebar/projectList/ProjectItem.tsx
+++ src/components/sidebar/projectList/ProjectItem.tsx
@@ -1,14 +1,18 @@
 type ProjectProps = {
-  id: string;
   title: string;
   description?: string;
+  onClick?: () => void;
 };
 
-export default function ProjectItem({ id, title, description }: ProjectProps) {
+export default function ProjectItem({
+  title,
+  description,
+  onClick,
+}: ProjectProps) {
   return (
     <div
       className="flex flex-col rounded px-2 py-4 bg-main-1 hover:cursor-pointer"
-      key={id}
+      onClick={onClick}
     >
       <h3>{title}</h3>
       <p>{description}</p>
