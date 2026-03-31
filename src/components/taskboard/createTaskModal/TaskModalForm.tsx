29d622bc8ef7546c0df5fc94b7a1bde4ae171f0b
Leonid Budkov <leonid.budkov@gmail.com>
2026-03-31 15:29:24 +0200

    feat(task-board): add TaskModalForm

---
+++ src/components/taskboard/createTaskModal/TaskModalForm.tsx
@@ -0,0 +1,83 @@
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import {
+  TASK_PRIORITIES,
+  TASK_STATUSES,
+  TaskPriority,
+  TaskStatus,
+} from "@/domain/tasks";
+
+type TaskModalFormProps = {
+  status: TaskStatus;
+  onStatus: (v: TaskStatus) => void;
+  priority: TaskPriority;
+  onPriority: (v: TaskPriority) => void;
+};
+
+export default function TaskModalForm({
+  status,
+  onStatus,
+  priority,
+  onPriority,
+}: TaskModalFormProps) {
+  return (
+    <FieldGroup>
+      <Field>
+        <FieldLabel>Description</FieldLabel>
+        <Input
+          id="input-field-description"
+          type="text"
+          placeholder="Enter a Description"
+        />
+      </Field>
+      <Field>
+        <FieldLabel>Status</FieldLabel>
+        <Select
+          defaultValue={status}
+          onValueChange={(value) => onStatus(value as TaskStatus)}
+        >
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {TASK_STATUSES.map((s, i) => (
+                <SelectItem key={i} value={s}>
+                  {s.substring(0, 1).toUpperCase() + s.substring(1)}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </Field>
+      <Field>
+        <FieldLabel>Priority</FieldLabel>
+        <Select
+          defaultValue={priority}
+          onValueChange={(value) => onPriority(value as TaskPriority)}
+        >
+          <SelectTrigger>
+            <SelectValue />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              {TASK_PRIORITIES.map((p, i) => (
+                <SelectItem key={i} value={p}>
+                  {p.substring(0, 1).toUpperCase() + p.substring(1)}
+                </SelectItem>
+              ))}
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </Field>
+    </FieldGroup>
+  );
+}
