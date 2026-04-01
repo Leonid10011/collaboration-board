6339445b077fcf992c652b6185ca37dcadc1eff5
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-01 20:31:14 +0200

    feat(update-task-modal): add updateTask flow with UI

--- src/components/taskboard/UpdateTaskModal.tsx
+++ src/components/taskboard/UpdateTaskModal.tsx
@@ -1,26 +1,55 @@
 import { Task } from "@/domain/tasks";
 import ModalShell from "../modal/ModalShell";
+import UpdateTaskModalForm from "./updateTaskModal/UpdateTaskModalForm";
+import { useState } from "react";
+import { useTask } from "@/context/TaskContext";
+import { showError, showSuccess } from "@/lib/toast";
 
 type UpdateModalTask = {
-  task: Task;
+  selectedTask: Task;
   onModalClose: () => void;
 };
 
 export default function UpdateTaskModal({
-  task,
+  selectedTask,
   onModalClose,
 }: UpdateModalTask) {
   const handleClose = () => {
     onModalClose();
   };
+
+  const { patchTask } = useTask();
+
+  const [title, setTitle] = useState<string>(selectedTask.title);
+  const [description, setDescription] = useState<string>(
+    selectedTask.description ? selectedTask.description : "",
+  );
+
+  const handleConfirm = async () => {
+    try {
+      await patchTask(selectedTask.id, {
+        title: title,
+        description: description,
+      });
+      showSuccess("Task Updated!");
+    } catch (error) {
+      showError(`${error}`);
+    }
+  };
+
   return (
     <>
       <ModalShell
         onClose={handleClose}
         confirmLabel="Confirm"
-        onConfirm={() => {}}
+        onConfirm={handleConfirm}
       >
-        <div>task</div>
+        <UpdateTaskModalForm
+          title={title}
+          onTitleChange={setTitle}
+          description={description}
+          onDescriptionChange={setDescription}
+        />
       </ModalShell>
     </>
   );
