66bbc7cdfbaa56e1d032bc0df926eda1fec78b73
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-03 16:24:57 +0200

    feat(ui): add SurfaceItem reusable component

--- src/components/ui/surface/SurfaceItem.tsx
+++ src/components/ui/surface/SurfaceItem.tsx
@@ -12,4 +12,21 @@ function SurfaceItem({ className, ...props }: React.ComponentProps<"div">) {
   );
 }
 
-export { SurfaceItem };
+/**
+ * Row component for surface items. It is a flex container that arranges its children in a row and applies some padding and hover effects. The position of the row can be customized using the className prop.
+ * @param
+ * @returns
+ */
+function SurfaceRow({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      className={cn(
+        "flex flex-row items-center gap-1 rounded-md hover:cursor-pointer transition-colors duration-150 hover:bg-black/5 py-1 px-2",
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export { SurfaceItem, SurfaceRow };
