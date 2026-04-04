148ff3ee22734aaf7b024ae282343588588f9a27
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-04 14:49:32 +0200

    refactor(info-block): generlize InfoBlock even  more

--- src/components/ui/InfoBlock.tsx
+++ src/components/ui/InfoBlock.tsx
@@ -1,17 +1,18 @@
+import { cn } from "@/lib/utils";
 import React from "react";
 
-type InfoBlock = {
+interface InfoBlock extends React.ComponentProps<"div"> {
   title: string;
   children: React.ReactNode;
-};
+}
 
 /** This is a reusable component to show information.
  *   It shows a title followed by corresponding information inside a react component
  */
 
-export default function InfoBlock({ title, children }: InfoBlock) {
+export default function InfoBlock({ className, title, children }: InfoBlock) {
   return (
-    <div className="flex flex-col gap-2">
+    <div className={cn("flex flex-col gap-2", className)}>
       <p className="text-xs font-bold">{title}</p>
       {children}
     </div>
