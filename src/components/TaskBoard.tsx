a34b37606699c9bd972ad7f7c5f247249c42d73a
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-05 13:53:43 +0200

    feat(task-board): add task filter

--- src/components/TaskBoard.tsx
+++ src/components/TaskBoard.tsx
@@ -7,16 +7,25 @@ import { Task, TASK_STATUSES, TaskStatus } from "@/domain/tasks";
 import CreateTaskModal from "./taskboard/CreateTaskModal";
 import { useTask } from "@/context/TaskContext";
 import UpdateTaskModal from "./taskboard/UpdateTaskModal";
+import { useUser } from "@/context/UserContext";
+import { FilterItem } from "./taskboard/FilterItem";
+import {
+  FILTER_CONFIG,
+  FILTER_MODES,
+  FilterMode,
+} from "./taskboard/task-board.config";
 
 export default function TaskBoard() {
   const STATUS_COLORS = ["gray", "yellow", "green"] as const;
 
   const { projectTasks: tasks } = useTask();
   const { projectMembers: members } = useProject();
+  const { user } = useUser();
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const [isUpdateModalOpen, setIsUpdateModalOpen] = useState<boolean>(false);
   const [newTaskStatus, setNewTaskStatus] = useState<TaskStatus>("backlog");
   const [selectedTask, setSelectedTask] = useState<Task | null>(null);
+  const [filterMode, setFilterMode] = useState<FilterMode>("all");
 
   const handleNewTaskStatus = (status: TaskStatus) => {
     setNewTaskStatus(status);
@@ -41,35 +50,59 @@ export default function TaskBoard() {
     setSelectedTask(tmp);
   };
 
+  const handleFilterMode = (mode: FilterMode) => {
+    setFilterMode(mode);
+  };
+
   const memberMap = useMemo(() => {
     if (!members) return null;
-    return new Map(members.map((m) => [m.id, m]));
+    return new Map(members.map((member) => [member.id, member]));
   }, [members]);
 
-  const TaskByStatus = useMemo(() => {
+  const filteredTasks = useMemo(() => {
+    if (filterMode === "all") return tasks ?? [];
+
+    return (tasks ?? []).filter((task) => task.assgineeId === user?.id);
+  }, [tasks, filterMode, user]);
+
+  const tasksByStatus = useMemo(() => {
     const map: Record<TaskStatus, Task[]> = {
       backlog: [],
       in_progress: [],
       done: [],
     };
 
-    tasks?.forEach((t) => {
-      map[t.status].push(t);
+    filteredTasks?.forEach((task) => {
+      map[task.status].push(task);
     });
 
     return map;
-  }, [tasks]);
+  }, [filteredTasks]);
 
   return (
     <div className="flex flex-col flex-1 gap-4 px-4 py-2">
-      <h1>Task Board</h1>
+      <div className="flex flex-row gap-2">
+        {FILTER_MODES.map((mode, i) => (
+          <>
+            <FilterItem
+              key={mode}
+              icon={FILTER_CONFIG[mode].icon}
+              label={FILTER_CONFIG[mode].label}
+              onFilter={() => handleFilterMode(mode)}
+            />
+            {i < FILTER_MODES.length - 1 ? (
+              <div className="w-px bg-main-2" />
+            ) : null}
+          </>
+        ))}
+      </div>
       <div className="flex flex-row gap-x-8 h-full">
-        {TASK_STATUSES.map((c, i) => (
+        {TASK_STATUSES.map((status, i) => (
           <Column
-            key={c}
+            key={status}
             statusColor={STATUS_COLORS[i]}
-            status={c}
-            tasks={TaskByStatus[c]}
+            status={status}
+            tasks={tasksByStatus[status]}
             userMap={memberMap}
             onModalOpen={() => handleModalOpen(true)}
             onUpdateModalOpen={() => handleUpdateModalOpen(true)}
