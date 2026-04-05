928dc0bdfc15810864908b8542d1657bd5e9efd1
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-05 22:42:30 +0200

    feat(project-info): add title change flow

--- src/components/homepage/ProjectInfo.tsx
+++ src/components/homepage/ProjectInfo.tsx
@@ -3,9 +3,16 @@ import InfoBlock from "../ui/InfoBlock";
 type ProjectInfo = {
   projectTitle: string | null;
   role: string | null;
+  onTitleChange: (text: string) => void;
+  onBlur: () => void;
 };
 
-export default function ProjectInfo({ projectTitle, role }: ProjectInfo) {
+export default function ProjectInfo({
+  projectTitle,
+  role,
+  onTitleChange,
+  onBlur,
+}: ProjectInfo) {
   //Will be replaced
 
   return (
@@ -14,9 +21,13 @@ export default function ProjectInfo({ projectTitle, role }: ProjectInfo) {
         <span className="text-green-600 text-xl w-[100px]">
           {role ? role : "User"}
         </span>
-        <span className="text-meta text-lg font-semibold">
-          {projectTitle ? projectTitle : "No Project Selected"}
-        </span>
+        <input
+          className="text-meta text-lg font-semibold"
+          value={projectTitle ? projectTitle : "No Project Selected"}
+          onChange={(e) => onTitleChange(e.currentTarget.value)}
+          onBlur={onBlur}
+          disabled={role !== "admin"}
+        />
       </div>
     </InfoBlock>
   );
