177f482ccb77d3ec6697e9c4802ac0cb403debdd
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-06 12:13:08 +0200

    polish(sidebar): add styling to make sidebars appeal consistent

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
   };
 
   return (
-    <div className="flex flex-row py-2 px-2 border-b-2 border-solid bg-main-2 gap-16">
+    <div className="flex flex-row py-2 px-page-inline border-b-2 border-solid bg-main-2 gap-16">
       <div className="flex flex-row gap-16">
         <UserInfo
           userName={user ? user.userName : null}
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -19,7 +19,7 @@ export default function Sidebar() {
   };
 
   return (
-    <div className="flex flex-col px-2 py-4 w-[200px] gap-4 border-r-2 border-solid bg-main-2">
+    <div className="flex flex-col px-page-inline py-4 w-sidebar gap-4 border-r-2 border-solid bg-main-2">
       <InfoBlock title="Actions">
         <SurfaceRow className="text-sm" onClick={handleCreateProjectClick}>
           <Plus size={16} />
