679ad5ce7fbc900a12cf79efaf1532e5bd437d9b
Leonid Budkov <leonid.budkov@gmail.com>
2026-04-15 15:44:28 +0200

    feat(task-action): add changeTaskPriorityAction

---
+++ src/features/tasks/actions/change-priority-task.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { createSupabaseServerClient } from "@/lib/supabase/server";
+import { UpdateTaskSchema } from "../schema";
+import { TaskPriority } from "../types";
+import { updateTaskRepo } from "../data/update-task";
+import { revalidatePath } from "next/cache";
+
+export async function changeTaskPriorityAction(
+  taskId: string,
+  priority: TaskPriority,
+) {
+  const parsed = UpdateTaskSchema.safeParse({ priority });
+  if (!parsed.success) throw new Error(parsed.error.message);
+
+  const supabase = await createSupabaseServerClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  const result = await updateTaskRepo(taskId, parsed.data);
+
+  revalidatePath("/");
+
+  return result;
+}
